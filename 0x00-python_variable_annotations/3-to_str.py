8cebd2319df2bce8954a40d8f0f9cc5c26380761
Eman288 <emantamam107@gmail.com>
2024-10-11 00:50:50 +0000

    task3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+3. Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    function to return the string representaion for a fkloat
+    """
+    return repr(n)
