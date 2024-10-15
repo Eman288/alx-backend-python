7efadee5431880e86c39ed98da66f3396b6ea9f1
Eman288 <emantamam107@gmail.com>
2024-10-15 23:27:36 +0000

    task0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/use/bin/env python3
+"""a module to Write an asynchronous coroutine"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """a function that waits for a random
+    delay between 0 and max_delay"""
+    y = random.uniform(0, max_delay)
+    await asyncio.sleep(y)
+    return y
